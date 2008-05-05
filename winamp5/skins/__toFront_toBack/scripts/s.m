0fad9c1533443feec2123598195d2847e9f4c4a4
mpdeimos <mpdeimos@442da66a-194c-0410-a885-59fc1719c42d>
2008-05-05 10:56:29 +0000

    updated testSkin

--- winamp5/skins/__toFront_toBack/scripts/s.m
+++ winamp5/skins/__toFront_toBack/scripts/s.m
@@ -1,19 +1,22 @@
 #include <lib/std.mi>
 
 Global Button b1, b2;
+Global Group g1, g2;
 
 System.onScriptLoaded ()
 {
+	g1 = getScriptGroup().findObject("g1");
+	g2 = getScriptGroup().findObject("g2");
 	b1 = getScriptGroup().findObject("1");
 	b2 = getScriptGroup().findObject("2");
 }
 
 b1.onLeftClick ()
 {
-	b2.bringtoFront();
+	g2.bringtoFront();
 }
 
 b2.onLeftClick ()
 {
-	b2.bringtoback();
+	g2.bringtoBack();
 }
