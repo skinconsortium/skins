630811602a6a734aa55536f50280cffe48cbc95a
nieuwoudtpieter@gmail.com <nieuwoudtpieter@gmail.com@442da66a-194c-0410-a885-59fc1719c42d>
2008-08-10 22:48:24 +0000

    playlistpro #003

--- winamp5/Plugins/classicPro/engine/xui/PlaylistPro/PlaylistPro.m
+++ winamp5/Plugins/classicPro/engine/xui/PlaylistPro/PlaylistPro.m
@@ -1,15 +1,17 @@
 /*
-PlaylistPro build 003
+PlaylistPro build 004
 by pjn123 (www.skinconsortium.com)
 */
 
 #include <lib/std.mi>
 #include <lib/pldir.mi>
+
 Function resizeResults(int items);
 Function doSearch(String input);
 Function setSearchBox(boolean onOff);
 Function clearSearchBox();
 Function int getPlEntry(int search_item);
+Function String replaceString(string baseString, string toreplace, string replacedby);
 
 Global Group frameGroup, topbar;
 Global Edit searchBox;
@@ -81,17 +83,8 @@ System.onShowLayout(Layout _layout){
 	if(main_layout.isVisible()) results_layout.setXmlParam("ontop", "1");
 }
 
-/*main_layout.onSetVisible(boolean onOff){
-	if(onOff) results_layout.resize(fakeSB.clientToScreenX(fakeSB.getLeft()), fakeSB.clientToScreenY(fakeSB.getTop() + fakeSB.getHeight()),fakeSB.getWidth(),400);
-}*/
-
 frameGroup.onSetVisible(boolean onOff){
 	if(!onOff) clearSearchBox();
-	
-	/*if(onOff){
-		searchButtonXui.setXmlParam("x", integerToString(-1*fakeText.getAutoWidth()-4));
-		searchButtonXui.setXmlParam("w", integerToString(fakeText.getAutoWidth()+2));
-	}*/
 }
 
 searchButton.onLeftClick(){
@@ -130,21 +123,32 @@ doSearch(String input){
 		clearSearchBox();
 		return; 
 	}
+	
+	//search history stuff
+	String history = getPublicString("cPro.PlaylistPro.history", "");
+	history = replaceString(history, input, "");
+	history = replaceString(history, ";;", ";");
+	if(strleft(history, 1)==";") history = strright(history, strlen(history)-1);
+	if(strright(history, 1)==";") history = strleft(history, strlen(history)-1);
+	if(history=="") history= input;
+	else history= input+";"+history;
+	
+	String output = getToken(history, ";", 0);
+	for(int i = 1; i<15; i++){
+		if(getToken(history, ";", i)=="") break;
+		output+=";"+getToken(history, ";", i);
+	}
+	setPublicString("cPro.PlaylistPro.history", history);
+
 
 	int itemsfound = 0;
 	input = strlower(input);
 	
-	//if(!results_layout.isVisible()){
-	//results_layout.resize(fakeSB.clientToScreenX(fakeSB.getLeft()), fakeSB.clientToScreenY(fakeSB.getTop() + fakeSB.getHeight()),fakeSB.getWidth(),1);
-
 	results_layout.setXmlParam("x", integerToString(fakeSB.clientToScreenX(fakeSB.getLeft())));
 	results_layout.setXmlParam("y", integerToString(fakeSB.clientToScreenY(fakeSB.getTop() + fakeSB.getHeight())));
 	results_layout.setXmlParam("w", integerToString(frameGroup.getWidth()-13));
 	
 	if(!results_layout.isVisible()) results_layout.setXmlParam("h", "1");
-
-	//}
-	//else results_layout.hide();
 
 	searchResults.deleteAllItems();
 	searchResults.scrollToItem(0);
@@ -174,55 +178,28 @@ doSearch(String input){
 			itemsfound++;
 			searchResults.addItem(integerToString(i+1)+". " + PlEdit.getTitle(i));
 			if(itemsfound>500){
-				searchNews.setText("** SEARCH LIMITED TO 500 ITEMS **");
-				//searchResults.addItem("** SEARCH LIMITED TO 500 ITEMS **");
+				searchNews.setText("Search result limited to 500 items");
 				break;
 			}
 		}
-
-		
-		/*if(strsearch(strlower(PlEdit.getTitle(i)), input)!=-1){
-			itemsfound++;
-			
-			searchResults.addItem(integerToString(i+1)+". " + PlEdit.getTitle(i));
-			//searchResults.setSubItem(searchResults.getLastAddedItemPos(), 1, integerToString(PlEdit.getLength (i)));
-			
-			//searchResults.addItem(integerToString(i));
-			//searchResults.addItem("a");
-			if(itemsfound>500){
-				searchResults.addItem("** SEARCH LIMITED TO 500 ITEMS **");
-				break;
-			}
-		}*/
 	}
 
 	if(itemsfound==0){
-		searchNews.setText("** Nothing found **");
-		//searchResults.addItem("** Nothing found **");
+		searchNews.setText("Nothing was found");
 	}
 	else if(itemsfound<=500){
-		if(itemsfound>1){
-			searchNews.setText("** Found "+integerToString(itemsfound)+" items **");
-			//searchResults.addItem("** Found "+integerToString(itemsfound)+" items **");
-		}
-		else{
-			searchNews.setText("** Found 1 item **");
-			//searchResults.addItem("** Found 1 item **");
-		}
+		searchNews.setText(System.translate("Items found: ") +integerToString(itemsfound));
 	}
 	resizeResults(itemsfound);
 
 	// Fix if always on top is enabled.. it just refresh the ontop ;)
-	//results_layout.setXmlParam("ontop", "0");
 	results_layout.setXmlParam("ontop", "1");
 
 	results_layout.show();
 }
 
 searchResults.onDoubleClick(Int itemnum){
-	//PlEdit.showTrack(stringToInteger(getToken(searchResults.getItemLabel(itemnum,0), ". ", 0))-1);
 	PlEdit.playTrack (getPlEntry(itemnum));
-	results_layout.hide();
 	setSearchBox(false);
 }
 
@@ -230,11 +207,12 @@ searchResults.onRightClick(Int itemnum){
 	search_rc = new PopupMenu;
 	search_rc.addCommand("Move selected to top", 1, 0, 0);
 	search_rc.addCommand("Move selected to bottom", 2, 0, 0);
-	search_rc.addCommand("Move selected after current", 3, 0, 1);
+	search_rc.addCommand("Move selected after current", 3, 0, 0);
 	search_rc.addCommand("Move selected together", 4, 0, 0);
 	search_rc.addSeparator();
-	search_rc.addCommand("Remove selected from playlist", 5, 0, 1);
+	search_rc.addCommand("Remove selected from playlist", 5, 0, 0);
 	int result = search_rc.popAtMouse();
+	delete search_rc;
 	
 	int lastselected = searchResults.getFirstItemSelected();
 	int itemcounter = 1;
@@ -257,6 +235,33 @@ searchResults.onRightClick(Int itemnum){
 		}
 		PlEdit.showTrack(PlEdit.getNumTracks ()-1);
 	}
+	else if(result==3){
+		int align = 0;
+		int orignalPos = PlEdit.getCurrentIndex();
+		int temp = getPlEntry(lastselected);
+		if(PlEdit.getCurrentIndex() > temp) align++;
+
+		if(orignalPos > getPlEntry(lastselected)) PlEdit.moveTo (getPlEntry(lastselected), PlEdit.getCurrentIndex());
+		else  PlEdit.moveTo (getPlEntry(lastselected), PlEdit.getCurrentIndex()+1);
+		
+		while(searchResults.getNextItemSelected(lastselected) != -1){
+			lastselected = searchResults.getNextItemSelected(lastselected);
+			
+			if(orignalPos != getPlEntry(lastselected)){
+				if(orignalPos > getPlEntry(lastselected)){
+					PlEdit.moveTo (getPlEntry(lastselected)-align, PlEdit.getCurrentIndex()+itemcounter);
+				}
+				else{
+					PlEdit.moveTo (getPlEntry(lastselected), PlEdit.getCurrentIndex()+itemcounter+1);
+				}
+
+				temp = getPlEntry(lastselected);
+				if(orignalPos > temp) align++;
+				itemcounter++;
+			}
+		}
+		PlEdit.showCurrentlyPlayingTrack();
+	}
 	else if(result==4){
 		int startpos = getPlEntry(lastselected);
 		while(searchResults.getNextItemSelected(lastselected) != -1){
@@ -266,14 +271,18 @@ searchResults.onRightClick(Int itemnum){
 		}
 		PlEdit.showTrack(startpos);
 	}
-
-	/*
-	extern Int GuiList.getFirstItemSelected();
-	extern Int GuiList.getNextItemSelected(Int lastpos);
-	*/
+	else if(result==5){
+		PlEdit.removeTrack (getPlEntry(lastselected));
+		while(searchResults.getNextItemSelected(lastselected) != -1){
+			lastselected = searchResults.getNextItemSelected(lastselected);
+			PlEdit.removeTrack (getPlEntry(lastselected)-itemcounter);	
+			itemcounter++;
+		}
+	}
+	
+	else return;
 	
 	//hides the search
-	results_layout.hide();
 	setSearchBox(false);
 }
 
@@ -283,7 +292,6 @@ int getPlEntry(int search_item){
 
 setSearchBox(boolean onOff){
 	if(onOff){
-		//searchXUI.show();
 		searchBox.show();
 		clearButton.show();
 		clearButtonText.show();
@@ -295,32 +303,53 @@ setSearchBox(boolean onOff){
 		clearButton.hide();
 		clearButtonText.hide();
 		helpSearch.show();
-		//searchXUI.hide();
+		results_layout.hide();
 	}
 }
+
 fakeSB.onLeftButtonDown(int x, int y){
 	setSearchBox(true);
 }
+fakeSB.onRightButtonDown(int x, int y){
+	search_rc = new PopupMenu;
+	search_rc.addCommand("** Search history **", 0, 0, 1);
+	search_rc.addSeparator();
+	
+	String history = getPublicString("cPro.PlaylistPro.history", "");
+	boolean historyfound;
+	for(int i=0;i<15;i++){
+		String historyitem = getToken(history, ";", i);
+		if(historyitem==""){
+			if(i>0) historyfound=true;
+			break;
+		}
+		search_rc.addCommand(historyitem, i+1, 0, 0);
+	}
+	if(historyfound) search_rc.addSeparator();
+	search_rc.addCommand("Clear History", 100, 0, 0);
+
+	int result = search_rc.popAtMouse();
+	delete search_rc;
+
+	if(result>0 && result<100){
+		setSearchBox(true);
+		searchBox.setText(getToken(history, ";", result-1));
+		searchButton.leftClick();
+	}
+	else if(result==100){
+		setPublicString("cPro.PlaylistPro.history", "");
+	}
+}
+
 clearSearchBox(){
 	searchBox.setText("");
-	setSearchBox(false);
-	results_layout.hide();
-}
-searchXUI.onKillFocus(){
-	//if(searchBox.getText()=="")
-	setSearchBox(false);
-}
-results_layout.onKillFocus(){
-	//if(searchBox.getText()=="")
 	setSearchBox(false);
 }
 
 main_layout.onMove(){
-	//results_layout.resize(fakeSB.clientToScreenX(fakeSB.getLeft()), fakeSB.clientToScreenY(fakeSB.getTop() + fakeSB.getHeight()),fakeSB.getWidth(),400);
 	if(results_layout.isVisible()){
 		results_layout.setXmlParam("x", integerToString(fakeSB.clientToScreenX(fakeSB.getLeft())));
 		results_layout.setXmlParam("y", integerToString(fakeSB.clientToScreenY(fakeSB.getTop() + fakeSB.getHeight())));
-		//results_layout.setXmlParam("ontop", "0");
 		results_layout.setXmlParam("ontop", "1");
 	}
 }
@@ -345,4 +374,18 @@ frameGroup.onResize(int x, int y, int w, int h){
 
 clearButton.onLeftClick(){
 	clearSearchBox();
+}
+
+String replaceString(string baseString, string toreplace, string replacedby){
+	if (toreplace == "") return baseString;
+	string sf1 = strupper(baseString);
+	string sf2 = strupper(toreplace);
+	int i = strsearch(sf1, sf2);
+	if (i == -1) return baseString;
+	string left = "", right = "";
+	if (i != 0) left = strleft(baseString, i);
+	if (strlen(basestring) - i - strlen(toreplace) != 0) {
+		right = strright(basestring, strlen(basestring) - i - strlen(toreplace));
+	}
+	return left + replacedby + right;
 }
