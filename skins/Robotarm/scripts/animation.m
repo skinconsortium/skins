6e8a4a92183a9e723562457b955fc44244d78933
Martin Pöhlmann <mpdeimos@users.noreply.github.com>
2008-08-25 09:02:31 +0000

    added display animation

---
+++ skins/Robotarm/scripts/animation.m
@@ -0,0 +1,93 @@
+/*---------------------------------------------------
+-----------------------------------------------------
+Filename:	animation.m
+Version:	1.0
+
+Type:		maki
+Date:		25. Aug. 2008 - 16:54 
+Author:		Martin Poehlmann aka Deimos
+E-Mail:		martin@skinconsortium.com
+Internet:	www.skinconsortium.com
+-----------------------------------------------------
+---------------------------------------------------*/
+
+#include <lib/std.mi>
+
+Function fade (GuiObject g, int alpha, float speed);
+Function toggleArm ();
+
+Global AnimatedLayer robotArm;
+Global Button animButton;
+Global Layout normal;
+Global Boolean armIsOut;
+Global Group display;
+
+System.onScriptLoaded ()
+{
+	robotArm = getScriptGroup().findObject("animationlayer");
+	animButton = getScriptGroup().findObject("animationbutton");
+	display =  getScriptGroup().findObject("robotarm.display");
+	normal = getScriptGroup().getParentLayout();
+}
+
+normal.onSetVisible (Boolean onoff)
+{
+	if (onoff)
+	{
+		toggleArm();
+	}
+}
+
+animButton.onLeftClick ()
+{
+	toggleArm();
+}
+
+
+toggleArm ()
+{
+	if (robotArm.isPlaying())
+	{
+		return;
+	}
+
+	if (armIsOut && display.isVisible())
+	{
+		fade (display, 0, 0.5);
+		return;
+	}
+
+	robotarm.play();
+}
+
+robotArm.onStop ()
+{
+	int ef = robotArm.getEndFrame();
+	robotArm.setEndFrame(robotArm.getStartFrame());
+	robotArm.setStartFrame(ef);
+	armIsOut = !armIsOut;
+
+	if (armIsOut)
+	{
+		fade (display, 255, 0.5);	
+	}
+}
+
+fade (GuiObject g, int alpha, float speed)
+{
+	g.canceltarget();
+	g.show();
+	g.setTargetA(alpha);
+	g.setTargetSpeed(speed);
+	g.gotoTarget();	
+}
+
+display.onTargetReached ()
+{
+	if (display.getAlpha() == 0)
+	{
+		display.hide();
+		toggleArm();
+	}
+	
+}
