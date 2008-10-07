d3f0135e4dcaf770753fbbaed535d8f279604224
Leechbite <leechbite@gmail.com>
2008-10-07 02:40:52 +0000

    added scare function. more to come.

--- skins/intruder2/scripts/scare.m
+++ skins/intruder2/scripts/scare.m
@@ -2,28 +2,79 @@
 
 #include <lib/std.mi>
 
-global layout mainnormal;
+function string pathToURL(string path);
+
+global layout mainnormal,scarelayout;
 global button timer30,timer60,timer90;
 
-global timer scaretimer;
+global timer scaretimer, loadtimer;
+
+global string scaremusic;
 
 System.onScriptLoaded() {
+	scaremusic = pathToURL(getParam());
+	
 	mainnormal = getContainer("main").getLayout("normalc");
+	
+	scarelayout = getContainer("scare").getLayout("normal");
+	scarelayout.hide();
+	scarelayout.resize(0,0,getMonitorWidth(),getMonitorHeight());
 	
 	timer30 = mainnormal.findObject("30sec");
 	timer60 = mainnormal.findObject("60sec");
 	timer90 = mainnormal.findObject("90sec");
 	
 	scaretimer = new timer;
+	
+	loadtimer = new timer;
+	loadtimer.setdelay(100);
+	loadtimer.start();
+}
+
+loadtimer.onTimer() {
+	stop();
+	
+	scarelayout.hide();
 }
 
 system.onScriptUnloading() {
 	delete scaretimer;
+	delete loadtimer;
+}
+
+string pathToURL(string path) {
+	int c, len = strlen(path);
+	string ret = "", char;
+
+	
+	c=0;
+	while (c < len) {
+		char = strmid(path,c,1);
+		if (char==chr(92)) {
+			ret = ret + chr(47);
+			c ++;
+		} else if (char==":") { //for ":/"
+			ret = ret + ":";
+			c ++;
+			
+			char = strmid(path,c,1);
+			if (char==chr(92)) {
+				ret = ret + chr(47) + chr(47);
+				c ++;
+			}
+		} else {
+			ret = ret + char;
+			c++;
+		}
+	}
+	
+	return ret;
 }
 
 timer30.onActivate(int on) {
 	if (!on) { scaretimer.stop(); return; }
 	
+	System.minimizeApplication();
 	timer60.setActivatedNoCallback(0);
 	timer90.setActivatedNoCallback(0);
 	
@@ -34,6 +85,7 @@ timer30.onActivate(int on) {
 timer60.onActivate(int on) {
 	if (!on) { scaretimer.stop(); return; }
 	
+	System.minimizeApplication();
 	timer30.setActivatedNoCallback(0);
 	timer90.setActivatedNoCallback(0);
 	
@@ -44,6 +96,7 @@ timer60.onActivate(int on) {
 timer90.onActivate(int on) {
 	if (!on) { scaretimer.stop(); return; }
 	
+	System.minimizeApplication();
 	timer60.setActivatedNoCallback(0);
 	timer30.setActivatedNoCallback(0);
 	
@@ -58,6 +111,18 @@ scaretimer.onTimer() {
 	timer60.setActivatedNoCallback(0);
 	timer90.setActivatedNoCallback(0);
 	
-	System.minimizeApplication();
+	scarelayout.resize(0,0,getMonitorWidth(),getMonitorHeight());
+	scarelayout.show();
+	
+	playfile(scaremusic);
+}
 
+System.onKeyDown(string key) {
+	if (!scarelayout.isVisible()) return;
+
+	if (key=="esc") {
+		scarelayout.hide();
+		stop();
+	}
+	
 }
