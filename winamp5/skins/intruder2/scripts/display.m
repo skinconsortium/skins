060c3bab3a708216e1f41741bdef6b22ac262820
leechbite <leechbite@442da66a-194c-0410-a885-59fc1719c42d>
2008-10-09 02:08:38 +0000

    added: fade on mouse out. added: fire on cbuttons.

--- winamp5/skins/intruder2/scripts/display.m
+++ winamp5/skins/intruder2/scripts/display.m
@@ -1,13 +1,16 @@
 #include <lib/std.mi>
 
-class guiobject grpObjs;
+class guiobject cbuttonobj;
 
 Global Group grp;
 
 Global layout parentLayout;
 
-Global Timer hold, mousechecker, hidetimer;
-Global grpObjs t;
+Global Timer hold, mousechecker;
+Global cbuttonobj cbuttons; //bplay,bstop,bnext,bprev,beject;
+Global layer fireButtons;
+
+Global int inArea = 0;
 
 function fadeinout(guiobject g, int alph, double speed);
 
@@ -16,7 +19,7 @@ function hideGrpObjs();
 
 system.onScriptLoaded() {	
 
-  	parentLayout = getContainer("main").getLayout("normal"); // grp.getParentLayout();
+  	parentLayout = getContainer("main").getLayout("normal");
   	
   	grp = parentLayout.findObject("main.control.buttons");
   	grp.hide();
@@ -28,15 +31,30 @@ system.onScriptLoaded() {
 	hold = new Timer;
 	hold.setDelay(1000);
 	
-	hidetimer = new Timer;
-	hidetimer.setDelay(1000);
+	fireButtons = parentLayout.findObject("anim.button");
+	
+	cbuttons = parentLayout.findObject("stop");
+	cbuttons = parentLayout.findObject("next");
+	cbuttons = parentLayout.findObject("rev");
+	cbuttons = parentLayout.findObject("eject");
+	cbuttons = parentLayout.findObject("play");
+}
+
+cbuttonobj.onEnterArea() {
+	fireButtons.setXMLParam("x",integerToString(getGuiX()-36+getWidth()/2));
+	fireButtons.setXMLParam("y",integerToString(getGuiY()-67+getHeight()/2));
+	fireButtons.show();
+}
+
+cbuttonobj.onLeaveArea() {
+	fireButtons.hide();
 }
 
 System.onScriptUnloading() {
 
 	delete hold;
 	delete mousechecker;
-	delete hidetimer;
+	
 
 }
 
@@ -47,18 +65,25 @@ showGrpObjs() {
 	for (c=0; c<max; c++) {
 		temp = NULL;
 		temp = grp.enumObject(c);
-		if (temp!=NULL) {temp.cancelTarget(); temp.setAlpha(255); }
+		if (temp!=NULL) { temp.cancelTarget(); temp.setAlpha(255); }
 	}
 }
 
 hideGrpObjs() {
 	int c = 0, max = grp.getNumObjects();
 	guiobject temp;
+	string tid;
 	
 	for (c=0; c<max; c++) {
-		t = NULL;
-		t = grp.enumObject(c);
-		if (t!=NULL) fadeinout(t, 0, 1.0);
+		temp = NULL;
+		temp = grp.enumObject(c);
+		if (temp!=NULL) {
+			tid = strupper(temp.getID());
+			//if (tid == "PLAY" || tid == "REV")
+				fadeinout(temp, 0, 0.4);
+			//else
+				//temp.setAlpha(0);
+		}
 	}
 }
 
@@ -67,14 +92,7 @@ hold.onTimer() {
 	hold.stop();
 	
 	hideGrpObjs();
-	
-	hidetimer.start();
-}
 
-hidetimer.onTimer() {
-	stop();
-	
-	grp.hide();
 }
 
 mousechecker.onTimer()
@@ -88,15 +106,19 @@ mousechecker.onTimer()
 	mousey = mousey - parentLayout.getTop();
 	
 	if (mousex > parentLayout.getWidth() || mousex < 0 || mousey > parentLayout.getHeight() || mousey < 0) {
-		if (grp.isVisible()) {
+		if (inArea) {
 			if (!hold.isRunning()) hold.start();
+
+			inArea = 0;
 		}
 	} else {
 		if (hold.isRunning()) hold.stop();
-		if (!grp.isVisible()) {
-			
+		if (!inArea) {
+				
 			grp.show();
 			showGrpObjs();
+
+			inArea = 1;
 		}
 	}
 	
