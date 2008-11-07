c6e2ed3e05454f48a645b0549352c4f491f312e5
Pieter Nieuwoudt <nieuwoudtpieter@gmail.com>
2008-11-07 21:28:23 +0000

    some progress :D

--- skins/centroV2/CproTab.m
+++ skins/centroV2/CproTab.m
@@ -1,12 +1,25 @@
 #include <lib/std.mi>
 
-Global Group myGroup, tab123;
+Global Group myGroup, tab123, workWithTab;
 
 System.onScriptLoaded() {
 	myGroup = getScriptGroup();
 	
-	tab123 = System.newGroup("cpro.tab");
-	tab123.setXmlParam("y", "50");
-	tab123.setXmlParam("tabtext", "VideoHaha");
-	tab123.init(myGroup);
+	
+	int createNtabs = 5;
+	
+	//how to create them
+	for(int i=0;i<createNtabs;i++){
+		tab123 = System.newGroup("cpro.tab");
+		tab123.setXmlParam("tabtext", "MyTab"+integerToString(i));
+		tab123.init(myGroup);
+		tab123.show();
+	}
+	
+	
+	//how to work with them
+	for(int i=1;i<createNtabs;i++){
+		myGroup.enumObject(i).setXmlParam("x", integerToString(myGroup.enumObject(i-1).getGuiX() + myGroup.enumObject(i-1).getWidth()));
+	}
+
 }
