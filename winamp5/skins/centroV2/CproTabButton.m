f3e43af7dd769e7b28eba11d14302482c72f2526
nieuwoudtpieter@gmail.com <nieuwoudtpieter@gmail.com@442da66a-194c-0410-a885-59fc1719c42d>
2008-11-08 11:03:12 +0000

    cprotabs  making more progress: - now know when a tab wants to move

--- winamp5/skins/centroV2/CproTabButton.m
+++ winamp5/skins/centroV2/CproTabButton.m
@@ -2,24 +2,29 @@
 
 Function buttonState(int mode);  //1=activated , 2=normal , 3=hover
 
-Global Group myGroup;
+Global Group myGroup, myParent;
 Global ToggleButton myButton;
 Global Text myText;
 Global GuiObject myGrid;
-Global int downX;
-Global boolean tabMouseDown, wasActive;
+Global int downX, tab_id;
+Global boolean tabMouseDown, wasActive, tabMoveSend;
 
 System.onScriptLoaded() {
 	myGroup = getScriptGroup();
+	myParent = myGroup.getParent();
 	myButton = myGroup.findObject("cpro.tab.button");
 	myGrid = myGroup.findObject("cpro.tab.grid");
 	myText = myGroup.findObject("cpro.tab.text");
+	tabMoveSend=false;
 }
 
 System.onSetXuiParam(String param, String value) {
 	if(strlower(param) == "tabtext"){
 		myText.setText(value);
 		myGroup.setXmlParam("w", integerToString(myText.getAutoWidth()+14));
+	}
+	else if(strlower(param) == "tab_id"){
+		tab_id = stringToInteger(value);
 	}
 }
 
@@ -56,7 +61,8 @@ myButton.onLeftClick(){
 	if(downX < x+4 && downX > x-4) myButton.setActivated(true);
 	else{
 		myButton.setActivated(wasActive);
-		debug(integertostring(downX) + " "+integertostring(System.getMousePosX()));
+		//myParent.sendAction("move_tab", "", tab_id, 0, 0, 0);
+		//debug(integertostring(downX) + " "+integertostring(System.getMousePosX()));
 	}
 }
 
@@ -75,11 +81,25 @@ myButton.onLeaveArea(){
 
 myButton.onLeftButtonDown(int x, int y){
 	tabMouseDown=true;
+	tabMoveSend=false;
 	wasActive=myButton.getActivated();
 	downX=System.getMousePosX();
 }
 myButton.onLeftButtonUp(int x, int y){
 	if(tabMouseDown){
 		tabMouseDown=false;
+		if(tabMoveSend){
+			myParent.sendAction("move_tab_now", "", tab_id, 0, 0, 0);
+		}
 	}
+}
+myButton.onMouseMove(int x, int y){
+	if(tabMouseDown && !tabMoveSend){
+		int x = System.getMousePosX();
+		if(downX >= x+4 || downX <= x-4){
+			tabMoveSend=true;
+			myParent.sendAction("move_tab", "", tab_id, 0, 0, 0);
+		}
+	}
+
 }
