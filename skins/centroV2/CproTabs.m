3c6ef8bbd74c9962ce89ac9dbe9ba4e79596da5b
Pieter Nieuwoudt <nieuwoudtpieter@gmail.com>
2008-11-08 17:14:43 +0000

    now autoresize when with is too small

--- skins/centroV2/CproTabs.m
+++ skins/centroV2/CproTabs.m
@@ -2,17 +2,26 @@
 
 Class Group CProTabButton;
 
+Function refresh_X();
+Function refresh_W();
+Function int getTabsW();
+Function setMaxTabW(); //
+
 Global Group myGroup, tg, workWithTab;
 Global CProTabButton tab123;
 Global GuiObject moveIcon;
+Global int createNtabs, maxW;
+Global boolean maxW_Set, allTabsMaxView; //set this disabled when you add a new tab
 
 System.onScriptLoaded() {
 	myGroup = getScriptGroup();
 	tg = myGroup.findObject("cprotabs.buttons");
 	moveIcon = myGroup.findObject("moveicon");
 
-	int createNtabs = 5;
-	
+	createNtabs = 5;
+	maxW_Set=false;
+	allTabsMaxView=false;
+
 	//how to create them
 	for(int i=0;i<createNtabs;i++){
 		tab123 = System.newGroup("cpro.tab");
@@ -24,9 +33,10 @@ System.onScriptLoaded() {
 	
 	
 	//how to work with them
-	for(int i=1;i<createNtabs;i++){
+	/*for(int i=1;i<createNtabs;i++){
 		tg.enumObject(i).setXmlParam("x", integerToString(tg.enumObject(i-1).getGuiX() + tg.enumObject(i-1).getWidth()));
-	}
+	}*/
+	refresh_X();
 
 }
 
@@ -44,4 +54,62 @@ tg.onAction(String action, String param, int x, int y, int p1, int p2, GuiObject
 		moveIcon.hide();
 		//debug("This tab wants to move to position of the indicator now: " + integerToString(x));
 	}
+}
+
+refresh_X(){
+	for(int i=1;i<createNtabs;i++){
+		tg.enumObject(i).setXmlParam("x", integerToString(tg.enumObject(i-1).getGuiX() + tg.enumObject(i-1).getWidth()));
+	}
+
+}
+
+refresh_W(){
+	int w = tg.getWidth();
+	
+	if(maxW_Set && maxW<w && allTabsMaxView) return;	// Dont do calculations if theres enough space
+
+	for(int i=0;i<createNtabs;i++){
+		tg.enumObject(i).setXmlParam("viewmode", "0");
+		allTabsMaxView=true;
+	}
+	
+	/*
+	Set the maxW of the tabs so that we dont do all these calculations when there is enough space 
+	We know the max space now because the above for loop changed all the tabs to their longest view mode
+	*/
+	if(!maxW_Set){
+		maxW = getTabsW();
+		maxW_Set=true;
+	}
+	
+	boolean width_Okay = false;
+	
+	for(int a=1; a<3; a++){
+		for(int b=createNtabs-1; b>=0; b--){
+			if(getTabsW()>w){
+				tg.enumObject(b).setXmlParam("viewmode", integerToString(a));
+				allTabsMaxView=false;
+			}
+			else{
+				width_Okay = true;
+				break;
+			}
+		}
+		if(width_Okay) break;
+	}
+	refresh_X();
+}
+
+
+
+int getTabsW(){
+	int w = 0;
+	for(int i=0;i<createNtabs;i++){
+		w += tg.enumObject(i).getWidth();
+	}
+	return w;
+}
+
+tg.onResize(int x, int y, int w, int h){
+	refresh_W();
 }
