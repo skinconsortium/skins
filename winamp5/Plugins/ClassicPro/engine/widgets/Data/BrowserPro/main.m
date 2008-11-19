7b0400ec1557c937229195507c52bb8545064d88
nieuwoudtpieter@gmail.com <nieuwoudtpieter@gmail.com@442da66a-194c-0410-a885-59fc1719c42d>
2008-11-19 15:42:58 +0000

    browserpro v2

--- winamp5/Plugins/ClassicPro/engine/widgets/Data/BrowserPro/main.m
+++ winamp5/Plugins/ClassicPro/engine/widgets/Data/BrowserPro/main.m
@@ -5,60 +5,64 @@
 #include <lib/application.mi>
 #include "convert_address.mi"
 
-Function refreshStuff();
+Function resizeResults(int items);
+//Function refreshStuff();
 Function addProvider(list paramname, list paramvalue);
 Function String replaceString(string baseString, string toreplace, string replacedby);
 Function int searchInListForItem(String input);
 Function selectSaved();
 Function surfSelected();
 Function initLoadFiles();
+Function updateDDList();
+Function ddlOpenClose();
 
 
-Global Layout mainLayout;
+Global Container results_container;
+Global Layout results_layout;
 
 Global Group myGroup;
 Global GuiList myList;
 Global XmlDoc myDoc;
-Global List loaded_P_Names, loaded_P_Url;
-Global int sourceNo;
+Global List loaded_P_Names, loaded_P_Url, loaded_P_Icons;
+Global int sourceNo, h_tune;
 Global boolean onetime;
-Global Text statusInfo;
+Global Text ddBoxText;
 Global ToggleButton enabledSwitch;
-Global Button menuOptions;
+Global Button menuOptions, dropListButton;
 Global PopUpMenu popMenu;
-Global GuiObject browserXUI;
+Global GuiObject browserXUI, fakeSB;
+Global Browser myBrowser;
+Global Layer ddlMouseT, ddlIcon;
 
 System.onScriptLoaded(){
 	setPublicInt("ClassicPro.BrowserPro.loaded", 1);
 	initWidget();
 	
-	mainLayout = getContainer("main").getLayout("normal");
-	browserXUI = mainLayout.findObject("cpro.browser");
+	results_container = newDynamicContainer("browserpro");
+	results_layout = results_container.getLayout("normal");
+
 	myGroup = getScriptGroup();
-	myList = myGroup.findObject("bp.servicelist");
-	statusInfo = myGroup.findObject("bp.statusbar");
-	enabledSwitch = myGroup.findObject("bp.onoff");
-	menuOptions = myGroup.findObject("bp.options");
+	myList = results_layout.findObject("BrowserPro.list");
+	ddBoxText = myGroup.findObject("browserpro.ddl.text");
+	///enabledSwitch = myGroup.findObject("bp.onoff");
+	menuOptions = myGroup.findObject("cpro.widget.browserpro.options");
+	dropListButton = myGroup.findObject("dropdownlist.button");
+	fakeSB = myGroup.findObject("browserpro.ddlist");
+	myBrowser = myGroup.findObject("browserpro.browser");
+	ddlMouseT = myGroup.findObject("browserpro.ddl.mousetrap");
+	ddlIcon = myGroup.findObject("browserpro.ddl.icon");
 	
 
 	myList.setIconWidth(16);
 	myList.setShowIcons(1);
 	sourceNo=0;
-	enabledSwitch.setActivated(getPublicInt("ClassicPro.BrowserPro.enabled", 0));
+	//enabledSwitch.setActivated(getPublicInt("ClassicPro.BrowserPro.enabled", 0));
 }
 
-System.onShowLayout(Layout _layout){
-	mainLayout = getContainer("main").getLayout("normal");
-	browserXUI = mainLayout.findObject("cpro.browser");
-	if(mainLayout==_layout){
-		if(getPublicInt("cPro.lastComponentPage", 0)==3 && getPublicInt("ClassicPro.BrowserPro.enabled", 0)){
-			initLoadFiles();
-			surfSelected();
-		}
-		/*else if(getPublicInt("ClassicPro.BrowserPro.opentab", 1) && getPublicInt("ClassicPro.BrowserPro.enabled", 0)){
-			initLoadFiles();
-			surfSelected();
-		}*/
+myGroup.onSetVisible(boolean onOff){
+	if(onOff){
+		initLoadFiles();
+		surfSelected();
 	}
 }
 
@@ -69,27 +73,13 @@ System.onScriptUnloading (){
 	delete loaded_P_Url;
 }
 
-enabledSwitch.onToggle(boolean onOff){
-	setPublicInt("ClassicPro.BrowserPro.enabled", onOff);
-	if(onOff && (browserXUI.isVisible() || getPublicInt("ClassicPro.BrowserPro.opentab", 0))) surfSelected();
-}
-
-browserXUI.onSetVisible(boolean onOff){
-	if(onOff && getPublicInt("ClassicPro.BrowserPro.enabled", 0)){
-		surfSelected();
-	}
-}
-
-myGroup.onSetVisible(boolean onOff){
-	if(onOff && !onetime){
-		initLoadFiles();
-	}
-}
 
 initLoadFiles(){
 	if(!onetime){
 		loaded_P_Names = new List;
 		loaded_P_Url = new List;
+		loaded_P_Icons = new List;
+		
 		myDoc = new XmlDoc;
 		
 		String temp = Application.GetApplicationPath()+"\Plugins\classicPro\engine\widgets\Data\BrowserPro\source.xml";
@@ -99,9 +89,7 @@ initLoadFiles(){
 		myDoc.parser_start();
 		myDoc.parser_destroy();
 		delete myDoc;
-		
-		//myList.setAutoSort(1);
-		//myList.setXmlParam("sort", "1");
+
 		selectSaved();
 
 		onetime=true;
@@ -122,6 +110,7 @@ addProvider(list paramname, list paramvalue){
 		}
 		else if(strlower(paramname.enumItem(i))=="icon"){
 			myList.setItemIcon(sourceNo, paramvalue.enumItem(i));
+			loaded_P_Icons.addItem(paramvalue.enumItem(i));
 		}		
 	}
 	loaded_P_Names.addItem(myList.getItemLabel(sourceNo, 0));
@@ -134,88 +123,71 @@ myDoc.parser_onCallback (String xmlpath, String xmltag, list paramname, list par
 	}
 }
 
-myList.onDoubleClick(Int itemnum){
-	String myUrl = loaded_P_Url.enumItem(loaded_P_Names.findItem(myList.getItemLabel(itemnum, 0)));
-	gotoBrowserUrl(prepareCustomUrl(myUrl));
+resizeResults(int items){
+	if(items>22) items=22;
+	if(items>1) h_tune=24;
+	else h_tune=19;
+
+	results_layout.setXmlParam("h", integerToString(h_tune+items*17));
 }
 
-myList.onLeftClick(Int itemnum){
-	setPublicString("ClassicPro.BrowserPro", myList.getItemLabel(itemnum, 0));
+dropListButton.onLeftClick(){
+	ddlOpenClose();
+}
+ddlMouseT.onLeftButtonUp(int x, int y){
+	ddlOpenClose();
+}
 
-	String myUrl = loaded_P_Url.enumItem(loaded_P_Names.findItem(myList.getItemLabel(itemnum, 0)));
-	setPublicString("ClassicPro.BrowserPro.lastselected", myUrl);
-}
-myList.onColumnLabelClick(Int col, Int x, Int y){
-	selectSaved();
-}
-myList.onItemSelection(Int itemnum, Int selected){
-	if(selected){
-		statusInfo.setText("Selected auto search: " + myList.getItemLabel(itemnum, 0));
+ddlOpenClose(){
+	if(results_layout.isVisible()) results_layout.hide();
+	else{
+		results_layout.setXmlParam("x", integerToString(fakeSB.clientToScreenX(fakeSB.getLeft())));
+		results_layout.setXmlParam("y", integerToString(fakeSB.clientToScreenY(fakeSB.getTop() + fakeSB.getHeight())));
+		results_layout.setXmlParam("w", integerToString(fakeSB.getWidth()));
+
+	
+		results_layout.show();
+		resizeResults(loaded_P_Names.getNumItems());
+		results_layout.setXmlParam("ontop", "1");
 	}
 }
 
-String replaceString(string baseString, string toreplace, string replacedby) {
-	if (toreplace == "") return baseString;
-	string sf1 = strupper(baseString);
-	string sf2 = strupper(toreplace);
-	int i = strsearch(sf1, sf2);
-	if (i == -1) return baseString;
-	string left = "", right = "";
-	if (i != 0) left = strleft(baseString, i);
-	if (strlen(basestring) - i - strlen(toreplace) != 0) {
-		right = strright(basestring, strlen(basestring) - i - strlen(toreplace));
-	}
-	return left + replacedby + right;
+selectSaved(){
+	int i = searchInListForItem(getPublicString("ClassicPro.BrowserPro", "0"));
+	myList.setSelected(i,1);
+	updateDDList();
 }
-
 searchInListForItem(String input){
 	for(int i=0; i<myList.getNumItems();i++){
 		if(input == myList.getItemLabel(i, 0)) return i;
 	}
 	return 0; //if nothing was found
 }
-
-selectSaved(){
-	int i = searchInListForItem(getPublicString("ClassicPro.BrowserPro", "0"));
-	myList.setSelected(i,1);
-}
-
 surfSelected(){
-	if(getContainer("main").getCurLayout() != mainLayout) return;
-
-	cpro_sui = getContainer("main").getLayout("normal").findObject("cpro.sui");
 	String myUrl = loaded_P_Url.enumItem(loaded_P_Names.findItem(getPublicString("ClassicPro.BrowserPro", "0")));
-	gotoBrowserUrl(prepareCustomUrl(myUrl));
-	
-	
+	myBrowser.navigateUrl(prepareCustomUrl(myUrl));
+
 	//testing download
 	//System.downloadMedia("", "C:/", false, false);
 }
-
 System.onTitleChange(String newtitle){
-	initLoadFiles();
-	if(getPublicInt("ClassicPro.BrowserPro.enabled", 0)){
-		if(getPublicInt("ClassicPro.BrowserPro.opentab", 0)){
-			surfSelected();
-		}
-		else if(getPublicInt("cPro.lastComponentPage", 0)==3){
-			surfSelected();
-		}
+	if(myGroup.isVisible()){
+		surfSelected();
 	}
 }
+myList.onLeftClick(Int itemnum){
+	setPublicString("ClassicPro.BrowserPro", myList.getItemLabel(itemnum, 0));
+	surfSelected();
+	results_layout.hide();
+	updateDDList();
+}
 
-menuOptions.onLeftClick(){
-	popMenu = new PopUpMenu;
-	popMenu.addCommand("Open browser if it's closed", 1, getPublicInt("ClassicPro.BrowserPro.opentab", 0), 0);
+updateDDList(){
+	ddBoxText.setText(getPublicString("ClassicPro.BrowserPro", "0"));
+	String iconsID = loaded_P_Icons.enumItem(loaded_P_Names.findItem(getPublicString("ClassicPro.BrowserPro", "0")));
+	ddlIcon.setXmlParam("image", iconsID);
+}
 
-	int result = popMenu.popAtXY(clientToScreenX(menuOptions.getLeft()), clientToScreenY(menuOptions.getTop() + menuOptions.getHeight()));
-
-	if(result==1){
-		setPublicInt("ClassicPro.BrowserPro.opentab", !getPublicInt("ClassicPro.BrowserPro.opentab", 0));
-	}
-
-	delete popMenu;
-	complete;
-
-
+results_layout.onKillFocus(){
+	debug("asdas");
 }
