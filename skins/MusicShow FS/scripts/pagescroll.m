f49cdfd6a2bebe1e098867c9fe360812547df661
Leechbite <leechbite@gmail.com>
2008-11-27 02:15:06 +0000

    -few updates

---
+++ skins/MusicShow FS/scripts/pagescroll.m
@@ -0,0 +1,24 @@
+/*************************************************************
+
+  pagescroll.m
+  
+  -script to control the page scrolling
+  
+
+*************************************************************/
+
+#include <lib/std.mi>
+
+global layout scriptGroup;
+
+
+
+System.onScriptLoaded() {
+
+	
+}
+
+System.onScriptUnloading() {
+	
+}
+
