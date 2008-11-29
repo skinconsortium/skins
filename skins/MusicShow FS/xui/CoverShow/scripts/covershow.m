1ad2198ef292a655a560d569987dcade2665556b
Leechbite <leechbite@gmail.com>
2008-11-29 05:46:23 +0000

    -left page scroll implemented -few updates on covershow

--- skins/MusicShow FS/xui/CoverShow/scripts/covershow.m
+++ skins/MusicShow FS/xui/CoverShow/scripts/covershow.m
@@ -21,6 +21,7 @@
 
 function update();
 function updateDim();
+function updateCover(group g, string source);
 function setAAgroupToPos(group g, float pos);
 
 #define NUMCOVERS = 2;
@@ -32,7 +33,8 @@ global group scriptGroup;
 Global group gprev2, gprev1, gcurr, gnext1, gnext2;
 Global AlbumCover prev2, prev1, curr, next1, next2;
 
-Global float currPos = 0;
+Global float currPos = 0, aaWidth;
+global int scw, sch, eyeDist;
 
 System.onScriptLoaded () {
 	scriptGroup = getScriptGroup();
@@ -58,64 +60,101 @@ System.onScriptLoaded () {
 	prev1sd.fx_setEnabled(0);
 	prev1sd.fx_setAlphaMode(1);
 	prev1sd.fx_setBilinear(1);*/
-
+	
 	update();
+	
+	eyeDist = 100;
+	aaWidth = 80;
+	
+	scriptGroup.onResize(0,0,scriptGroup.getWidth(),scriptGroup.getHeight()); // sets 3D variables;
+	
+
 }
 
-System.onTitleChange (String newtitle)
-{
+System.onTitleChange (String newtitle) {
 	update();
+	//updateDim();
+}
+
+scriptGroup.onResize(int x, int y, int w, int h) {
+	scw = w;
+	sch	= h;
+	
+	eyeDist = h/2;
+	aaWidth = w/4;
+	
+	updateDim();
+}
+
+updateCover(group g, string source) {
+	layer aa = g.getObject("aa");
+	layer aaref = g.getObject("aa.ref");
+	
+	aa.setXmlParam("source", source);
+	aaref.setXmlParam("source", source);
 }
 
 update () {
 	int cur = PlEdit.getCurrentIndex();
 	int max = PlEdit.getNumTracks();
 
-	if (cur > 1) 
-	{
-		prev2.setXmlParam("source", PlEdit.getFileName(cur-2));
-		prev2.show();
+	if (cur > 1)  {
+		updateCover(gprev2, PlEdit.getFileName(cur-2));
+		gprev2.show();
 	}
-	else	prev2.hide();
+	else gprev2.hide();
 
-	if (cur > 0)
-	{
-		prev1.setXmlParam("source", PlEdit.getFileName(cur-1));
-		prev1.show();
+	if (cur > 0) {
+		updateCover(gprev1, PlEdit.getFileName(cur-1));
+		gprev1.show();
 	}
-	else	prev1.hide();
+	else prev1.hide();
 	
-	if (cur < max-2) 
-	{
-		next2.setXmlParam("source", PlEdit.getFileName(cur+2));
-		next2.show();
+	updateCover(gcurr, PlEdit.getFileName(cur));
+	
+	if (cur < max-2) {
+		updateCover(gnext2, PlEdit.getFileName(cur+2));
+		gnext2.show();
 	}
-	else	next2.hide();
+	else gnext2.hide();
 
-	if (cur < max-1)
-	{
-		next1.setXmlParam("source", PlEdit.getFileName(cur+1));
-		next1.show();
+	if (cur < max-1) {
+		updateCover(gnext1, PlEdit.getFileName(cur+1));
+		gnext1.show();
 	}
-	else	next1.hide();
+	else gnext1.hide();
 	
 	currPos = cur;
-	updateDim();
 	/*prev1sd.fx_setEnabled(0);
 	prev1sd.fx_setEnabled(1);
 	prev1sd.fx_restart();*/
 }
 
 updateDim() {
-
+	
 	setAAgroupToPos(gprev2, -2);
+	setAAgroupToPos(gprev1, -1);
+	//setAAgroupToPos(gcurr, 0);
 }
 
 setAAgroupToPos(group g, float pos) {
-	int w = scriptGroup.getWidth();
-	int h = scriptGroup.getHeight();
+	int xmid = scw/2;
+	int x1 = pos*aaWidth;
+	int y1 = x1;
+	if (y1 < 0) y1 = -y1;
 	
-	//int ax = w
+	double rat = eyeDist/(y1+eyeDist);
+	int xp = x1*rat;
+		
+	int w = aaWidth*rat;
+
+	int h = w*1.667;
+	int yp = (sch - h)/2;
+
+	g.setXMLParam("x",integerToString(xmid+xp));
+	g.setXMLParam("y",integerToString(yp));
+	g.setXMLParam("w",integerToString(w));
+	g.setXMLParam("h",integerToString(h));
 }
 
 /*
