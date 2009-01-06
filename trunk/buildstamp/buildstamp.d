6d16ee716646fd6204e6f126bc37349d58cc1a8b
mpdeimos <mpdeimos@442da66a-194c-0410-a885-59fc1719c42d>
2009-01-06 20:49:20 +0000

    buildstamp: a nice tool for automated managing of buildstamps

---
+++ trunk/buildstamp/buildstamp.d
@@ -0,0 +1,11 @@
+/**
+ * !!! DO NOT MODIFY THIS FILE !!!
+ * This file has been created by buildstamp v0.1 build 8
+ * (c) 2009 Martin Poehlmann
+ * http://www.skinconsortium.com
+ */
+
+module buildstamp;
+
+public const uint buildstamp_uint = 9;
+public const char[] buildstamp_char = "9";
