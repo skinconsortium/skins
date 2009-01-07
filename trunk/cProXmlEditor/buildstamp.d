3a8f0b63f50695aaa9a1d35d55f003cc83dad739
mpdeimos <mpdeimos@442da66a-194c-0410-a885-59fc1719c42d>
2009-01-07 16:40:49 +0000

    finishing eclipse migration

--- trunk/cProXmlEditor/buildstamp.d
+++ trunk/cProXmlEditor/buildstamp.d
@@ -1,11 +1,11 @@
 /**
  * !!! DO NOT MODIFY THIS FILE !!!
- * This file has been created by buildstamp v0.1 build 5
+ * This file has been created by buildstamp v0.1 build 28
  * (c) 2009 Martin Poehlmann
  * http://www.skinconsortium.com
  */
 
 module buildstamp;
 
-public const uint buildstamp_uint = 10;
-public const char[] buildstamp_char = "10";
+public const uint buildstamp_uint = 22;
+public const char[] buildstamp_char = "22";
