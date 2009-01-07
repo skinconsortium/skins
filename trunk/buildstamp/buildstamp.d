19b0fc8b2c8ab67fa88a724bee351c8b23c23594
mpdeimos <mpdeimos@442da66a-194c-0410-a885-59fc1719c42d>
2009-01-07 16:45:13 +0000

    finishing eclipse migration

--- trunk/buildstamp/buildstamp.d
+++ trunk/buildstamp/buildstamp.d
@@ -1,11 +1,11 @@
 /**
  * !!! DO NOT MODIFY THIS FILE !!!
- * This file has been created by buildstamp v0.1 build 16
+ * This file has been created by buildstamp v0.1 build 27
  * (c) 2009 Martin Poehlmann
  * http://www.skinconsortium.com
  */
 
 module buildstamp;
 
-public const uint buildstamp_uint = 17;
-public const char[] buildstamp_char = "17";
+public const uint buildstamp_uint = 28;
+public const char[] buildstamp_char = "28";
