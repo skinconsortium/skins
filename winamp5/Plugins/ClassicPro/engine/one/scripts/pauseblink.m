81852e149b6ea9755e7646b3a762d146efbd4d08
nieuwoudtpieter <nieuwoudtpieter@442da66a-194c-0410-a885-59fc1719c42d>
2009-01-11 12:17:04 +0000

    Forgot to add the scripts for the previous build ;)

---
+++ winamp5/Plugins/ClassicPro/engine/one/scripts/pauseblink.m
@@ -0,0 +1,70 @@
+#include <lib/std.mi>
+
+Function doFade();
+Function stopFade();
+Function startFade();
+
+Global Group mainGroup;
+Global Text tracktimer;
+Global Boolean direction;
+Global Timer myTimer;
+
+System.onScriptLoaded() {
+	mainGroup = getScriptGroup();
+	tracktimer = mainGroup.findObject("SongTime");
+
+	myTimer = new Timer;
+	myTimer.setDelay(700);
+	
+	if(System.getStatus()==STATUS_PAUSED){
+		startFade();
+	}
+}
+
+System.onScriptUnloading(){
+	delete myTimer;
+}
+
+
+
+myTimer.onTimer(){
+	doFade();
+}
+
+
+System.onStop(){
+	stopFade();
+}
+System.onPlay(){
+	stopFade();
+}
+System.onResume(){
+	stopFade();
+}
+System.onPause(){
+	startFade();
+}
+
+doFade(){
+	if(direction){
+		tracktimer.setTargetA(0);
+	}
+	else{
+		tracktimer.setTargetA(253);
+	}
+	tracktimer.setTargetSpeed(0.6);
+	tracktimer.gotoTarget();
+	direction=!direction;
+}
+
+startFade(){
+	direction=true;
+	doFade();
+	myTimer.start();
+}
+
+stopFade(){
+	myTimer.stop();
+	tracktimer.cancelTarget();
+	tracktimer.setAlpha(253);
+}
