54ff3f74630249402dd0f56bdd35cba91aff52b5
mpdeimos <mpdeimos@442da66a-194c-0410-a885-59fc1719c42d>
2009-03-13 22:11:38 +0000

    cpro.w5s updates

--- winamp5/trunk/classicpro_w5s/global.h
+++ winamp5/trunk/classicpro_w5s/global.h
@@ -4,4 +4,11 @@
 #define CPRO_VERSION 0.1f
 #define CPRO_FLEX_XML L"classicpro.xml"
 
+
+#ifdef DEBUG
+#define WARN(msg) MessageBox(0, msg, L"Warning", 0)
+#else
+#define WARN(msg)
+#endif
+
 #endif
