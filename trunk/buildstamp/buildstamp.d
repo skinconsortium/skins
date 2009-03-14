ab8be95f470b8e4492f9def04a4117c536e12dca
mpdeimos <mpdeimos@442da66a-194c-0410-a885-59fc1719c42d>
2009-03-14 14:49:35 +0000

    latest buildstamp tool

--- trunk/buildstamp/buildstamp.d
+++ trunk/buildstamp/buildstamp.d
@@ -1,11 +1,11 @@
 /**
  * !!! DO NOT MODIFY THIS FILE !!!
- * This file has been created by buildstamp v0.1 build 27
+ * This file has been created by buildstamp v0.1 build 28
  * (c) 2009 Martin Poehlmann
  * http://www.skinconsortium.com
  */
 
 module buildstamp;
 
-public const uint buildstamp_uint = 28;
-public const char[] buildstamp_char = "28";
+public const uint buildstamp_uint = 29;
+public const char[] buildstamp_char = "29";
