7d4a67de96498a72aea3aa6942103dbd0ff5abce
nieuwoudtpieter <nieuwoudtpieter@442da66a-194c-0410-a885-59fc1719c42d>
2009-06-06 14:12:10 +0000

    - warning updated for v1.12

--- winamp5/Plugins/ClassicPro/engine/one/scripts/warning_1.12.m
+++ winamp5/Plugins/ClassicPro/engine/one/scripts/warning_1.12.m
@@ -15,7 +15,7 @@ System.onScriptLoaded() {
 		System.switchSkin("winamp classic");	// just switch away from our current skin
 	}
 	else{
-		if(getCproVersion()<111){
+		if(getCproVersion()<112){
 			myTimer = new Timer;
 			myTimer.setDelay(5000);
 			myTimer.start();
