30ee94b9806493bb8385ab7b4871f99fd161f0d6
mpdeimos@gmail.com <mpdeimos@gmail.com@442da66a-194c-0410-a885-59fc1719c42d>
2009-08-03 14:24:32 +0000

    updating WCF projects

--- trunk/wcf/me.songbook.wcf.startsitePIPtest/install.sql
+++
@@ -1,6 +0,0 @@
--- creates a dummy table so wcf is satisfied :P
-
-CREATE TABLE wcf1_optionals_dummy (
-	packageID int(11) UNSIGNED NOT NULL,
-	PRIMARY KEY  (packageID)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8
