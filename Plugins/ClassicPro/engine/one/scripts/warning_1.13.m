562f18b0f279503044c8a77b55255250e3b3b0b5
Pieter Nieuwoudt <nieuwoudtpieter@gmail.com>
2009-09-07 20:13:40 +0000

    [cpro] Release minor updates v1.13

--- Plugins/ClassicPro/engine/one/scripts/warning_1.13.m
+++ Plugins/ClassicPro/engine/one/scripts/warning_1.13.m
@@ -15,7 +15,7 @@ System.onScriptLoaded() {
 		System.switchSkin("winamp classic");	// just switch away from our current skin
 	}
 	else{
-		if(getCproVersion()<112){
+		if(getCproVersion()<113){
 			myTimer = new Timer;
 			myTimer.setDelay(5000);
 			myTimer.start();
