79f3e21583a11b16edff953def24cd84203b9df6
nieuwoudtpieter@gmail.com <nieuwoudtpieter@gmail.com@442da66a-194c-0410-a885-59fc1719c42d>
2009-09-24 08:38:58 +0000

    [cpro] Widget Manager 2 wip

---
+++ winamp5/Plugins/ClassicPro/engine/scripts/widgetManItem.m
@@ -0,0 +1,104 @@
+/**
+ * Handles Widget Manager Items
+ *
+ * @author mpdeimos
+ * @date 2009/04/28
+ */
+
+#include <lib/std.mi>
+#include "../lib/ClassicProFile.mi"
+
+Global Text name, author, version;
+Global Layer thumbnail;
+Global String thumbnailImg, uninstallerPath, widgetID, supportURL;
+Global Button uninstaller, support;
+Global Group sg;
+
+Global Button showmini, showdrawer, showmain;
+
+System.onScriptLoaded ()
+{
+	sg = getScriptGroup();
+
+	author = sg.getObject("author");
+	version = sg.getObject("version");
+	name = sg.getObject("name");
+
+	thumbnail = sg.getObject("thumbnail");
+	thumbnailImg = thumbnail.getXmlParam("image");
+	
+	uninstaller = sg.getObject("uninstall");
+	support = sg.getObject("support");
+	
+	showmini = sg.getObject("show.mini");
+	showdrawer = sg.getObject("show.drawer");
+	showmain = sg.getObject("show.main");
+
+	uninstallerPath = "";
+}
+
+system.onSetXuiParam (String param, String value)
+{
+	if (strlower(param) == "widgetname")
+	{
+		name.setText(value);
+		version.setXmlParam("x", integerToString(65 + name.getTextWidth()));
+	}
+	else if (strlower(param) == "widgetpos_mini"){
+		showmini.setXmlParam("ghost", "0");
+		showmini.setAlpha(255);
+	}
+	else if (strlower(param) == "widgetpos_drawer"){
+		showdrawer.setXmlParam("ghost", "0");
+		showdrawer.setAlpha(255);
+	}
+	else if (strlower(param) == "widgetpos_main"){
+		showmain.setXmlParam("ghost", "0");
+		showmain.setAlpha(255);
+	}
+	else if (strlower(param) == "widgetauthor")
+	{
+		author.setText("by "+value);
+	}
+	else if (strlower(param) == "widgetversion")
+	{
+		version.setText("v"+value);
+	}
+	else if (strlower(param) == "widgetid")
+	{
+		widgetID = value;
+		thumbnail.setXmlParam("image", value+".thumbnail");
+		if (thumbnail.isInvalid())
+		{
+			thumbnail.setXmlParam("image", thumbnailImg);
+		}
+		
+	}
+	else if (strlower(param) == "widgetuninstaller")
+	{
+			uninstallerPath = value;
+	}
+	else if (strlower(param) == "widgetsupport")
+	{
+			supportURL = value;
+	}
+}
+
+uninstaller.onLeftClick ()
+{
+	ClassicProFile.openFile(uninstallerPath, "");
+}
+support.onLeftClick ()
+{
+	System.navigateUrl(supportURL);
+	//debug(widgetID+" _ " + sg.getXmlParam("userdata"));
+	//getParentLayout().sendAction("show_widget", widgetID,stringToInteger(sg.getXmlParam("userdata")),0,0,0);
+}
+
+
+showmain.onLeftClick ()
+{
+	//debug(sg.getXmlParam("userdata"));
+	getParentLayout().sendAction("show_widget", widgetID,stringToInteger(sg.getXmlParam("userdata")),0,0,0);
+}
+
--- winamp5/Plugins/ClassicPro/engine/scripts/widgetsManager.m
+++ winamp5/Plugins/ClassicPro/engine/scripts/widgetsManager.m
@@ -15,15 +15,15 @@ Global Layout manager_normal;
 Class Group WidgetPlace;
 Member String WidgetPlace.name;
 
-Global List widgetPlaces;
-Global int done;
+Global List widgetPlaces, widgetList;
+Global int done, widgetType; //0=main, 1=drawer, 2=side
 
-Global GroupList grplst, grplstN;
-Global Slider vscroll, vscrollN;
+Global GroupList grplst;//,  grplstN;
+Global Slider vscroll;//, vscrollN;
 Global String widgetPath;
 
-Global Group grpAll, grpNew;
-Global Button newW, allW, dld1, dld2;
+Global Group grpAll;//, grpNew;
+//Global Button newW, allW, dld1, dld2;
 
 Global Boolean newWidgetInstalled, hasWidgets;
 
@@ -31,39 +31,118 @@ System.onScriptLoaded ()
 {
 	main_normal = getContainer("main").getLayout("normal");
 	manager_normal = getContainer("widgets.manager").getLayout("normal");
-	grpNew = manager_normal.findObject("widgets.manager.content.new");
+	//grpNew = manager_normal.findObject("widgets.manager.content.new");
 	grpAll = manager_normal.findObject("widgets.manager.content.all");
 
 	grplst = grpAll.findObject("grplst");
 	grplst.setRedraw(1);
-	grplstN = grpNew.findObject("grplst");
-	grplstN.setRedraw(1);
+	//grplstN = grpNew.findObject("grplst");
+	//grplstN.setRedraw(1);
 	vscroll = grpAll.findObject("vscroll");
-	vscrollN = grpNew.findObject("vscroll");
-	newW = manager_normal.findObject("newW");
-	allW = manager_normal.findObject("allW");
+	//vscrollN = grpNew.findObject("vscroll");
+	//newW = manager_normal.findObject("newW");
+	//allW = manager_normal.findObject("allW");
 	widgetPath = getParam();
 	widgetPlaces = new List;
+	widgetList = new List;
 	done = 0;
 	newWidgetInstalled = false;
 	hasWidgets = false;
 	
-	dld1 = grplstN.instantiate("widgets.manager.listitem.none", 1).getObject("dld");
-	dld2 = grplst.instantiate("widgets.manager.listitem.none", 1).getObject("dld");
+	//dld1 = grplstN.instantiate("widgets.manager.listitem.none", 1).getObject("dld");
+	//dld2 = grplst.instantiate("widgets.manager.listitem.none", 1).getObject("dld");
 }
 
-main_normal.onAction (String action, String param, Int x, int y, int p1, int p2, GuiObject _source)
+main_normal.onAction (String action, String param, int x, int y, int p1, int p2, GuiObject _source)
 {
+	//debugint(x);
 	if (strlower(action) == "widget_manager_register")
 	{
+		if(param=="Main Area") widgetType=0;
+		else if(param=="Drawer Area") widgetType=1;
+		else if(param=="Side Area") widgetType=2;
+		
+		/*
+		
+		debugstring("1: "+param,9);
 		complete;
 		WidgetPlace wp = _source;
 		wp.name = param;
 		widgetPlaces.addItem(wp);
 		return widgetPlaces.getNumItems()-1;
+		*/
 	}
 	else if (strlower(action) == "widget_manager_check")
 	{
+		//debug("2: "+param);
+		int widgetNo;
+		boolean createNewItem=false;
+		
+		if(widgetList.getNumItems()==0) createNewItem = true;
+		
+		for(int i = 0; i< widgetList.getNumItems(); i++){
+			if(widgetList.enumItem(i)==param){
+				widgetNo = i+1;
+				//debug(widgetList.enumItem(i) + " = " + param);
+				createNewItem = false;
+				break;
+			}
+			else if(i==widgetList.getNumItems()-1){
+				createNewItem = true;
+				break;
+			}
+			
+		}
+		
+		Group g = NULL;
+		if(createNewItem){
+
+			widgetList.addItem(param);
+			widgetNo = widgetList.getNumItems();
+	
+			GuiObject data = NULL;
+
+			g = grplst.instantiate("widgets.manager.listitem", 1);
+			Group source = _source;
+			g.setXmlParam("widgetname", getToken(source.getXmlParam("name"), ";", 0));
+
+			data = source.getObject("@version");
+			if(data) g.setXmlParam("widgetversion", data.getXmlParam("userdata"));
+
+			//data = NULL;
+			data = source.getObject("@author");
+			g.setXmlParam("widgetauthor", data.getXmlParam("userdata"));
+			if(data) g.setXmlParam("widgetid", param);
+
+			//data = NULL;
+			data = source.getObject("@uninstaller");
+			if(data) g.setXmlParam("widgetuninstaller", widgetPath + data.getXmlParam("userdata"));
+
+			//data = NULL;
+			data = source.getObject("@support");
+			if(data) g.setXmlParam("widgetsupport", data.getXmlParam("userdata"));
+
+			g.setXmlParam("userdata", integertostring(x));
+			
+		}
+		
+		
+		g = grplst.enumItem(widgetNo-1);
+		//if(g==Null) debug("123");
+		if(widgetType==0){
+			g.setXmlParam("widgetpos_main","1");
+		}
+		else if(widgetType==1){
+			g.setXmlParam("widgetpos_drawer","1");
+		}
+		else if(widgetType==2){
+			g.setXmlParam("widgetpos_mini","1");
+		}
+		g.setXmlParam("userdata", integertostring(x));
+		
+		
+		
+		/*debugstring("2: "+param,9);
 		WidgetPlace wp = widgetPlaces.enumItem(x);
 
 		Group source = _source;
@@ -135,13 +214,15 @@ main_normal.onAction (String action, String param, Int x, int y, int p1, int p2,
 		data = source.getObject("@support");
 		if (data)
 			g.setXmlParam("widgetsupport", data.getXmlParam("userdata"));
+			
+		*/
 	}
 	else if (strlower(action) == "widget_manager_done")
 	{
 		done++;
 		if (done == NUM_WIDGET_PLACES && newWidgetInstalled == true)
 		{
-			newW.onLeftClick();
+			//newW.onLeftClick();
 			manager_normal.show();
 		}
 		
@@ -179,15 +260,15 @@ vscroll.onSetPosition(int newpos)
 	grplst.setRedraw(percent);
 }
 
-vscrollN.onSetPosition(int newpos)
+/*vscrollN.onSetPosition(int newpos)
 {
 	int percent = 99-newpos;
 	//grplst.setXmlParam("y", integertostring(-newpos));
 	grplstN.scrolltopercent(percent);
 	grplstN.setRedraw(percent);
-}
+}*/
 
-newW.onLeftClick ()
+/*newW.onLeftClick ()
 {
 	grpAll.hide();
 	grpNew.show();
@@ -197,9 +278,9 @@ allW.onLeftClick ()
 {
 	grpNew.hide();
 	grpAll.show();
-}
+}*/
 
-dld1.onLeftClick ()
+/*dld1.onLeftClick ()
 {
 	System.navigateURL("http://www.skinconsortium.com/index.php?page=Downloads&typeID=7");
 }
@@ -207,4 +288,4 @@ dld1.onLeftClick ()
 dld2.onLeftClick ()
 {
 	System.navigateURL("http://www.skinconsortium.com/index.php?page=Downloads&typeID=7");
-}
+}*/
