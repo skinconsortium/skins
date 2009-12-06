09fcdb4e7481f996edf50ca5b0d184234413aca0
nieuwoudtpieter <nieuwoudtpieter@442da66a-194c-0410-a885-59fc1719c42d>
2009-12-06 11:58:29 +0000

    [cpro2] skin updates - think the layout is now taking form. Next need to implement buttons/sliders

--- winamp5/skins/CproTwo/scripts/playback-layout.m
+++ winamp5/skins/CproTwo/scripts/playback-layout.m
@@ -1,25 +1,50 @@
 #include <lib/std.mi>
 
-Global Group g, g_volume, g_vis;
+Global Group g, g_volume, g_vis, g_buttons;
+Global Layer l_left, l_right, l_butright;
 
 System.onScriptLoaded() {
 	g = getScriptGroup();
+	g_buttons = g.getObject("two.playback.buttons");
 	g_volume = g.getObject("two.playback.volume");
 	g_vis = g.getObject("two.playback.vis");
+	
+	l_butright = g_buttons.getObject("two.playback.buttons.right");
+	
+	l_left = g.getObject("two.playback.left");
+	l_right = g.getObject("two.playback.right");
 }
 
 g.onResize(int x, int y, int w, int h){
-	if(w<391){
+	if(w<406){
+		l_left.setXmlParam("image","playback.bg.left.1");
+		l_right.setXmlParam("image","playback.bg.right.1");
+		l_right.setXmlParam("x","-8");
+		l_butright.setXmlParam("image","playback.bg.buttons.right.1");
+		g_vis.hide();
 		g_volume.hide();
-		g_vis.hide();
 	}
-	else if(w<468){
+	else if(w<466){
+		l_left.setXmlParam("image","playback.bg.left.2");
+		l_right.setXmlParam("image","playback.bg.right.2");
+		l_right.setXmlParam("x","-91");
+		l_butright.setXmlParam("image","playback.bg.buttons.right.2");
+		g_vis.show();
 		g_volume.show();
-		g_vis.hide();
+		g_volume.setXmlParam("x","-91");
+		g_volume.setXmlParam("w","91");
 	}
 	else{
-		g_volume.show();
+		l_left.setXmlParam("image","playback.bg.left.2");
+		l_right.setXmlParam("image","playback.bg.right.2");
+		l_right.setXmlParam("x","-91");
+		l_butright.setXmlParam("image","playback.bg.buttons.right.2");
 		g_vis.show();
-		g_vis.setXmlParam("x", integerToString(165+(w-165-75-151)/2-39));
+		g_volume.show();
+		g_volume.setXmlParam("x","-121");
+		g_volume.setXmlParam("w","121");
 	}
+	
+	
+	g_buttons.setXmlParam("x", integerToString(w/2-112));
 }
