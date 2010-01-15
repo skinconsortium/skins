a513f72c93036ca6e1a0007c7dab64bb852e7904
nieuwoudtpieter <nieuwoudtpieter@442da66a-194c-0410-a885-59fc1719c42d>
2010-01-15 09:32:32 +0000

    [cpro2] more updates on skin

--- winamp5/skins/CproTwo/scripts/info-seeker.m
+++ winamp5/skins/CproTwo/scripts/info-seeker.m
@@ -1,20 +1,37 @@
 #include <lib/std.mi>
 
 Function setSeekerPos();
+Function lightToggle(boolean OnOff);
 
-Global Group g;
+Global Group g, g_seekerActive;
 Global Container c;
-Global Layer l_seekerActive, l_seekerInactive, l_seekerFinder;
-Global int i_titlebar, i_info, i_playback;
+Global Layer l_seekerActive, l_seekerInactive, l_seekerFinder, l_light;
+Global int i_light;
 Global Slider s_seeker0, s_seeker1;
+Global Timer t_light;
 
 System.onScriptLoaded() {
 	g = getScriptGroup();
 	s_seeker0 = g.getObject("two.info.seeker.slider.0");
 	s_seeker1 = g.getObject("two.info.seeker.slider.1");
 	l_seekerInactive = g.getObject("two.info.seeker.inactive");
-	l_seekerActive = g.getObject("two.info.seeker.active");
 	l_seekerFinder = g.getObject("two.info.seeker.finder");
+
+	g_seekerActive = g.getObject("two.info.seeker.active");
+	l_light = g_seekerActive.getObject("two.info.seeker.active.light");
+	l_seekerActive = g_seekerActive.getObject("two.info.seeker.active.layer");
+
+	Map m = new Map;
+	m.loadMap("info.bg.seeker.light");
+	i_light = m.getWidth();
+	delete m;
+	
+	t_light = new Timer;
+	t_light.setDelay(5000);
+	
+	if(System.getStatus()==STATUS_PLAYING){
+		lightToggle(true);
+	}
 }
 
 System.onScriptUnloading(){
@@ -47,7 +64,7 @@ setSeekerPos(){
 	
 	l_seekerInactive.setXmlParam("x", integerToString(temp*newpos/fullpos));
 	l_seekerInactive.setXmlParam("w", integerToString(temp-temp*newpos/fullpos));
-	l_seekerActive.setXmlParam("w", integerToString(temp*newpos/fullpos));
+	g_seekerActive.setXmlParam("w", integerToString(temp*newpos/fullpos));
 }
 
 
@@ -60,4 +77,40 @@ s_seeker1.onLeaveArea(){
 
 System.onStop(){
 	setSeekerPos();
+	lightToggle(false);
+}
+System.onPlay(){
+	lightToggle(true);
+}
+System.onPause(){
+	lightToggle(false);
+}
+
+System.onResume(){
+	lightToggle(true);
+}
+
+t_light.onTimer(){
+	l_light.cancelTarget();
+	l_light.setXmlParam("x", integerToString(-i_light));
+	l_light.show();
+	l_light.setTargetX(g.getWidth());
+	l_light.setTargetSpeed(1.5);
+	l_light.gotoTarget();
+	t_light.setDelay(5000);
+}
+
+l_light.onTargetReached(){
+	l_light.hide();
+}
+
+lightToggle(boolean OnOff){
+	if(OnOff){
+		t_light.setDelay(2000);
+		t_light.start();
+	}
+	else{
+		t_light.stop();
+		l_light.hide();
+	}
 }
