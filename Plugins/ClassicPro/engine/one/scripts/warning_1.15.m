47737fee599a70a3a953f3a8d03453e725ce4fbd
Pieter Nieuwoudt <nieuwoudtpieter@gmail.com>
2010-09-16 09:24:12 +0000

    [cpro] all the last changed for cpro1.15

--- Plugins/ClassicPro/engine/one/scripts/warning_1.15.m
+++ Plugins/ClassicPro/engine/one/scripts/warning_1.15.m
@@ -15,7 +15,7 @@ System.onScriptLoaded() {
 		System.switchSkin("winamp classic");	// just switch away from our current skin
 	}
 	else{
-		if(getCproVersion()<114){
+		if(getCproVersion()<115){
 			myTimer = new Timer;
 			myTimer.setDelay(5000);
 			myTimer.start();
